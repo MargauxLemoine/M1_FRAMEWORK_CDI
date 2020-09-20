da9107858147ce4df076e25adc371e0ecdede434
margaux <margaux@localhost>
2020-09-20 15:45:35 +0200

    Injection de dépendances par factory + manuelle

--- cdi-course/src/main/java/fr/pantheonsorbonne/ufr27/miage/exercise/Waiter.java
+++ cdi-course/src/main/java/fr/pantheonsorbonne/ufr27/miage/exercise/Waiter.java
@@ -1,15 +1,33 @@
 package fr.pantheonsorbonne.ufr27.miage.exercise;
 
 import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.BlackCoffee;
+import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.BlackCoffeeFactory;
 import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.Coffee;
 import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.ExpressoCoffee;
-import fr.pantheonsorbonne.ufr27.miage.exercise.machines.ExpressoMachine;
+import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.ExpressoCoffeeFactory;
 
 class Waiter {
+	
+	private final BlackCoffee c1;
+	private final ExpressoCoffee c2;
+	
+	public Waiter() {
+		c1 = BlackCoffeeFactory.getBlackCoffee();
+		c2 = ExpressoCoffeeFactory.getExpressoCoffee();
+	}
+	
+	public Waiter(BlackCoffee BlackCoffee, ExpressoCoffee ExpressoCoffee) {
+		this.c1 = BlackCoffee;
+		this.c2 = ExpressoCoffee;
+	}
 
-	public Coffee serveCoffee() {
+
+	public Coffee serveCoffee(Client client) {
 		
-		return new BlackCoffee();
+		if (client.getAwakenessLevel() + Math.max(c1.caffeineLevel(), c2.caffeineLevel()) <= 6)
+			return c1.caffeineLevel() > c2.caffeineLevel() ? c1 : c2;
+		else
+			return c1.caffeineLevel() < c2.caffeineLevel() ? c1 : c2;
 	}
 
 }
