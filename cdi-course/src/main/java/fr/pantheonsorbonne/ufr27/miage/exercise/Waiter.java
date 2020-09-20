d597274c9a9484871e624e411a7f6de670630fb8
margaux <margaux@localhost>
2020-09-20 17:18:50 +0200

    Maj injection de dépendances manuelle + Test unitaire

--- cdi-course/src/main/java/fr/pantheonsorbonne/ufr27/miage/exercise/Waiter.java
+++ cdi-course/src/main/java/fr/pantheonsorbonne/ufr27/miage/exercise/Waiter.java
@@ -5,29 +5,29 @@ import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.BlackCoffeeFactory;
 import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.Coffee;
 import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.ExpressoCoffee;
 import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.ExpressoCoffeeFactory;
+import fr.pantheonsorbonne.ufr27.miage.exercise.machines.CoffeeMachine;
 
 class Waiter {
 	
 	private final BlackCoffee c1;
 	private final ExpressoCoffee c2;
+	private final CoffeeMachine machine;
 	
 	public Waiter() {
 		c1 = BlackCoffeeFactory.getBlackCoffee();
 		c2 = ExpressoCoffeeFactory.getExpressoCoffee();
 	}
 	
-	public Waiter(BlackCoffee BlackCoffee, ExpressoCoffee ExpressoCoffee) {
+	public Waiter(BlackCoffee BlackCoffee, ExpressoCoffee ExpressoCoffee, CoffeeMachine machine) {
 		this.c1 = BlackCoffee;
 		this.c2 = ExpressoCoffee;
+		this.machine = machine;
 	}
 
 
 	public Coffee serveCoffee(Client client) {
 		
-		if (client.getAwakenessLevel() + Math.max(c1.caffeineLevel(), c2.caffeineLevel()) <= 6)
-			return c1.caffeineLevel() > c2.caffeineLevel() ? c1 : c2;
-		else
-			return c1.caffeineLevel() < c2.caffeineLevel() ? c1 : c2;
+		return machine.getCoffee();
 	}
 
 }
--- cdi-course/src/test/java/fr/pantheonsorbonne/ufr27/miage/exercise/AppTest.java
+++ cdi-course/src/test/java/fr/pantheonsorbonne/ufr27/miage/exercise/AppTest.java
@@ -5,6 +5,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.Coffee;
+import fr.pantheonsorbonne.ufr27.miage.exercise.machines.BlackCoffeeMachine;
+import fr.pantheonsorbonne.ufr27.miage.exercise.machines.CoffeeMachine;
+import fr.pantheonsorbonne.ufr27.miage.exercise.machines.ExpressoMachine;
+import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.BlackCoffee;
+import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.BlackCoffeeFactory;
+import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.Coffee;
+import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.ExpressoCoffee;
+import fr.pantheonsorbonne.ufr27.miage.exercise.coffee.ExpressoCoffeeFactory;
 
 public class AppTest {
 	
@@ -27,9 +35,19 @@ public class AppTest {
 	public void testCase1() {
 
 		try {
-			Waiter waiter = new Waiter();
+			
 			
 			Client client = new Client(-3);
+			
+			CoffeeMachine machine = null;
+			
+			if (client.getAwakenessLevel() > -15) {
+				machine = new BlackCoffeeMachine();
+			} else {
+				machine = new ExpressoMachine();
+			}
+			
+			Waiter waiter = new Waiter(BlackCoffee, ExpressoCoffee, machine);
 			
 			int profit = 3;
 
@@ -51,9 +69,19 @@ public class AppTest {
 	public void testCase2() {
 
 		try {
-			Waiter waiter = new Waiter();
 
 			Client client = new Client(-20);
+			
+CoffeeMachine machine = null;
+			
+			if (client.getAwakenessLevel() > -15) {
+				machine = new BlackCoffeeMachine();
+			} else {
+				machine = new ExpressoMachine();
+			}
+			
+			Waiter waiter = new Waiter(BlackCoffee, ExpressoCoffee, machine);
+			
 			int profit = 3;
 
 			while (!client.isAwake()) {
